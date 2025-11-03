d8836ae2344f91cc6145f3c1e8fcb79d0ccaf6c0
Måns Abrahamsson <mans.abrahamsson@gmail.com>
2025-11-03 22:19:36 +0100

    crab jump attack

--- characters/enemies/crab/enemy_climbing_ship_state.gd
+++ characters/enemies/crab/enemy_climbing_ship_state.gd
@@ -2,19 +2,24 @@ extends State
 
 @export var body:CharacterBody3D
 
-@export var speed = 10.0
+@export var speed = 2.0
 
-@export var target_height = 10.0
+@export var climb_speed = 5.0
+
+@export var target_z = 5.75
 
 @export var next_state: State
 
 
-#	For now, climb untill it reaches a target height
+#	For now, climb untill it reaches a target z
 #	later, this will be based on a collider on the ship
 # 	/ an Area3d on the ship!
 #
-
 func state_process(_delta:float):
-	body.velocity = Vector3.UP * speed
-	if(body.global_position.y > target_height):
+	body.velocity = (Vector3.FORWARD) * speed
+	
+	if(body.is_on_wall()):
+		body.velocity = (Vector3.UP) * climb_speed
+	
+	if(body.global_position.z < target_z):
 		try_set_state(next_state)
